159b766c1282f8805772dff5695006e2bc4c545c
walidsi <42148514+walidsi@users.noreply.github.com>
2022-12-01 17:02:39 +0200

    feat: Solved part 2 of day 1 puzzle

--- aoc_1.py
+++ aoc_1.py
@@ -2,16 +2,16 @@ def parse_calories_to_list() -> list:
     with open('aoc_1.txt') as f:
         lines = f.readlines()
 
-    elfves_calories = []
+    elves_calories = []
     elf_calories = []
     for line in lines:
         if line == '\n':
-            elfves_calories.append(elf_calories)
+            elves_calories.append(elf_calories)
             elf_calories = []
         else:
             elf_calories.append(int(line))
 
-    return elfves_calories
+    return elves_calories
 
 
 def calculate_max_elf_calories() -> int:
@@ -29,3 +29,19 @@ def calculate_max_elf_calories() -> int:
 
 max_sum_calories = calculate_max_elf_calories()
 print(max_sum_calories)
+
+
+def calculate_top_three_elf_calories() -> int:
+    elfves_calories = parse_calories_to_list()
+
+    total_elves_calories = []
+    for calories in elfves_calories:
+        total_elves_calories.append(sum(calories))
+
+    total_elves_calories = sorted(total_elves_calories, reverse=True)
+
+    return sum(total_elves_calories[0:3])
+
+
+sum_top_three = calculate_top_three_elf_calories()
+print(sum_top_three)

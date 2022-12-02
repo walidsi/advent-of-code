a14e437189f3085ce2181da5583f524e2b19f79a
walidsi <42148514+walidsi@users.noreply.github.com>
2022-12-02 11:30:47 +0200

    refactor: Concise solution using map() and reduce()

--- day1.py
+++ day1.py
@@ -1,5 +1,8 @@
+from functools import reduce
+
+
 def parse_calories_to_list() -> list:
-    with open('aoc_1.txt') as f:
+    with open('day1_input.txt') as f:
         lines = f.readlines()
 
     elves_calories = []
@@ -14,34 +17,12 @@ def parse_calories_to_list() -> list:
     return elves_calories
 
 
-def calculate_max_elf_calories() -> int:
-    elfves_calories = parse_calories_to_list()
+elfves_calories = parse_calories_to_list()
+total_elves_calories = list(map(lambda x: sum(x), elfves_calories))
 
-    max_sum_calories = 0
-
-    for calories in elfves_calories:
-        sum_calories = sum(calories)
-        if sum_calories > max_sum_calories:
-            max_sum_calories = sum_calories
-
-    return max_sum_calories
-
-
-max_sum_calories = calculate_max_elf_calories()
+max_sum_calories = reduce(lambda x, y: x if x > y else y, total_elves_calories)
 print(max_sum_calories)
 
-
-def calculate_top_three_elf_calories() -> int:
-    elfves_calories = parse_calories_to_list()
-
-    total_elves_calories = []
-    for calories in elfves_calories:
-        total_elves_calories.append(sum(calories))
-
-    total_elves_calories = sorted(total_elves_calories, reverse=True)
-
-    return sum(total_elves_calories[0:3])
-
-
-sum_top_three = calculate_top_three_elf_calories()
+total_elves_calories = sorted(total_elves_calories, reverse=True)
+sum_top_three = sum(total_elves_calories[0:3])
 print(sum_top_three)
